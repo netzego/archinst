2a1c6364a85b74cbdc6662dfcedf1ee5d2e0d247
netzego <github@netzego.de>
2023-05-21 22:23:48 +0200

    src: fix: change SCRIPTNAME to work without .ext

--- include/vars.bash
+++ include/vars.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 readonly PWD="$(pwd)"
-readonly SCRIPTNAME="$(basename "${BASH_ARGV0%.*}")"
+readonly SCRIPTNAME="$(basename "${BASH_ARGV0}")"
 readonly VERSION_NUMBER="0"
 readonly LOCKFILE="/tmp/${SCRIPTNAME}.lock"
 readonly CONFIGFILE="${PWD}/${SCRIPTNAME}.config"
