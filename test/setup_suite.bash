bc2a304a0956d5df42a704ad1a24bf5f7f6d80fa
netzego <github@netzego.de>
2023-05-25 18:45:16 +0200

    test: add test image setup/teardown funcs

--- test/setup_suite.bash
+++ test/setup_suite.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2154
+# shellcheck disable=SC2154,SC2034
 
 load "fixtures.bash"
 
@@ -8,4 +8,17 @@ load "fixtures.bash"
 # NOTE: this file is sourced/load by bats auto magically
 setup_suite() {
     bats_require_minimum_version 1.5.0
+
+    # test image
+    fixture_create_image
+    fixture_attach_image
+
+    export BATS_FIXTURE_IMAGE="${BATS_SUITE_TMPDIR}/test.img"
+    export BATS_FIXTURE_DEVICE="$(losetup -j "${BATS_FIXTURE_IMAGE}" | cut -d : -f 1)"
+}
+
+teardown_suite() {
+    # test image cleanup
+    fixture_detach_image
+    fixture_delete_image
 }
