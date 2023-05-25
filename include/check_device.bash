5f206dd3acab7f0bec51ed65f21cfc0af1769b46
netzego <github@netzego.de>
2023-05-25 18:46:47 +0200

    src: new check_device.bash

---
+++ include/check_device.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# DESC: check if $1 is a block device and if writeable else will exit with
+#       status code 1
+# ARGS: $1 path to a device
+check_device() {
+    local device="$1"
+
+    if [ ! -b "${device}" ]; then
+        die 1 "\`${device}' is not a block device"
+    fi
+
+    if [ ! -w "${device}" ]; then
+        die 1 "\`${device}' is not writable"
+    fi
+}
