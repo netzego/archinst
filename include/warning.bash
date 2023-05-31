476111c38850206d57ffe1b448533e5318e2aed8
netzego <github@netzego.de>
2023-05-31 11:24:13 +0200

    src: add warning func

---
+++ include/warning.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+warning() {
+    if [ "${BATCHMODE}" != true ]; then
+
+        print_header "${FUNCNAME[0]}"
+
+        warn "This will DESTROY all data in ${DEVICE}"
+
+        read -rp "${READ_PREFIX} Type \`yes' (in caps) to continue: " answer
+
+        if [ ! "${answer}" == "YES" ]; then
+            die 1 "stop execution"
+        fi
+    fi
+}
