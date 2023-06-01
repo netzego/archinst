fe026410be5738efe34317ed46a0e7cdff6a3cc0
netzego <github@netzego.de>
2023-06-01 15:31:04 +0200

    src: add print_header call

--- include/update_pacman.bash
+++ include/update_pacman.bash
@@ -3,6 +3,8 @@
 # DESC: update package database
 # ARGS: none
 update_pacman() {
+    print_header "${FUNCNAME[0]}"
+
     if ! pacman -Sy; then
         die 1 "could not update package database"
     fi
