235956b165d30c9e54288e1b7ad553243e4b1b3c
netzego <github@netzego.de>
2023-06-02 20:02:00 +0200

    src: new gen_cmdline.bash

---
+++ include/gen_cmdline.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+# shellcheck disable=SC2154
+
+# DESC: generate /etc/kernel/cmdline
+# ARGS: $1 (required): UUID of root partiton (eg. /dev/sdX2)
+#       $2 (required): UUID of luks partiton (eg. /dev/mapper/XXX)
+# COND: $WORKSPACE
+#       $MOUNTPOINT
+#       $CMDLINE_EXTRA
+# TODO: tests
+gen_cmdline() {
+    local root_uuid="$1"
+    local luks_uuid="$2"
+    local cmdline="rd.luks.name=${root_uuid}=rootfs root=UUID=${luks_uuid} rootflags=noatime,compress=zstd rw ${CMDLINE_EXTRA}"
+
+    print_header "${FUNCNAME[0]}"
+
+    echo "${cmdline}" | tee "${MOUNTPOINT}/etc/kernel/cmdline"
+}
