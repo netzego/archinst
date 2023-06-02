ec8bcb801889979b7457c083bd81a456e7f81e02
netzego <github@netzego.de>
2023-06-02 16:44:15 +0200

    src: add db.lck check

--- include/update_pacman.bash
+++ include/update_pacman.bash
@@ -5,6 +5,10 @@
 update_pacman() {
     print_header
 
+    if [ -f "/var/lib/pacman/db.lck" ]; then
+        die 1 "${FUNCNAME[0]}: db.lck exists"
+    fi
+
     if ! pacman -Sy; then
         die 1 "could not update package database"
     fi
