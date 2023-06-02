6cb4e6dd9bebbf7ac49739a9960943ac984f9c8f
netzego <github@netzego.de>
2023-06-02 13:07:14 +0200

    src: change to use curl instead ping

--- include/check_network.bash
+++ include/check_network.bash
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-# DESC: check if system is online by ping
+# DESC: check if host system is online with curl
 # ARGS: $1: host (dns name or ip address). default: archlinux.org
-#       $2: how many packets to send. default: 1
-#       $3: sec to wait before drop packet. default: 0.3
 # EXIT: if $1 is not reachable
 check_network() {
     local host="${1:-archlinux.org}"
-    local count="${2:-1}"
-    local timeout="${3:-0.3}"
 
-    print_header "${FUNCNAME[0]}"
+    print_header
 
-    ping -n -q -c "${count}" -W "${timeout}" "${host}" &>/dev/null
-
-    # shellcheck disable=SC2181
-    if [ $? -ne 0 ]; then
+    if ! curl --connect-timeout 0.5 -s -I "${host}"; then
         die 1 "\`${host}' is not reachable"
     fi
 }
