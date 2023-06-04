6b179a596ddc618f548ef326a1f6dbb1dff106ba
netzego <github@netzego.de>
2023-06-04 17:18:49 +0200

    src: bugfix undeclared $mountpoint var

--- include/copy_sshkeys.bash
+++ include/copy_sshkeys.bash
@@ -2,16 +2,18 @@
 # shellcheck disable=SC2154
 
 # DESC: copy archinst.pubkey to /root/.ssh/.authorized_keys
-# ARGS: none
+# ARGS: `$1` (optional): the path to a ssh authorized_keys file
+#       `$2` (optional): the path to the rootfs
 # COND: $WORKSPACE
 #       $MOUNTPOINT
-# TODO: checks, tests, naming
+# TODO: checks, tests
 copy_sshkeys() {
-    local pubkeyfile="${PWD}/${SCRIPTNAME}.pubkey"
+    declare -r keyfile="${1:-${PWD}/${SCRIPTNAME}.pubkey}"
+    declare -r rootfs="${2:-${MOUNTPOINT}}"
 
     print_header
 
-    if [[ -f "${pubkeyfile}" ]]; then
-        cp -v "${pubkeyfile}" "${mountpoint}/root/.ssh/authorized_keys"
+    if [[ -f "${keyfile}" ]]; then
+        cp -v "${keyfile}" "${rootfs}/root/.ssh/authorized_keys"
     fi
 }
